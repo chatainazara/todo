e27f7243d5d70cfa9d0a04bf492ae062fa9056c5
azarashi <montagnedog@yahoo.co.jp>
2025-07-08 21:45:09 +0900

    3-7.2

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,7 +15,7 @@
             </span>
             <!-- カテゴリー部分 -->
             <select class="form__input-category" name="cate11">
-                <option value="aaa" ></option>
+                <option value="" ></option>
                 @foreach($categories as $category)
                 <option value="{{$category['id']}}" >{{$category['name']}}</option>
                 @endforeach
